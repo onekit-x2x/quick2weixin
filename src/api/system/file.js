1eced4e22681f47d4ed5f643d50e2e222477b85f
15802303637 <>
2021-01-21 14:18:40 +0800

    .

---
+++ src/api/system/file.js
@@ -0,0 +1,330 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
+module.exports = {
+  /** file.move */
+  move() {
+    return console.log('move暂不支持！')
+  },
+  /** file.copy */
+
+  copy(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    var quick_srcUri = quick_object.srcUri
+    var quick_srcUri = quick_object.dstUri
+    // /////////////////////////
+    const wx_object = {}
+    if (quick_srcUri) {
+      wx_object.srcPath = quick_srcUri
+    }
+    if (quick_dstUri) {
+      wx_object.destPath = quick_srcUri
+    }
+    wx_object.success = function (wx_res) {
+      const quick_res = {}
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      const quick_res = {
+        fail: 202
+      }
+      for (const wx_res_key in wx_res) {
+        const wx_res_value = wx_res[wx_res_key]
+        switch (wx_res_key) {
+          case 'errMsg':
+            break
+        }
+      }
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().copyFile(wx_object)
+  },
+  /** file.list */
+
+  list(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    // /////////////////////////////////////////////
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.dirPath = quick_uri
+    }
+    wx_object.success = function (wx_res) {
+      const quick_res = {}
+      quick_res.fileList = wx_res.files
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().readdir(wx_object)
+  },
+  /** file.get */
+
+  get(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    // const quick_recursive = quick_object.recursive || false
+    // /////////////////////////////////////////////
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.filePath = quick_uri
+    }
+    wx_object.success = function (wx_res) {
+      const quick_res = {
+        uri: wx_object.filePath,
+        lastModifiedTime,
+        type,
+        subFiles
+      }
+      quick_res.length = wx_res.size
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().getFileInfo(wx_object)
+  },
+  /** file.delete */
+
+  delete(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.filePath = quick_uri
+    }
+    wx_object.success = function () {
+      const quick_res = {}
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().unlink(wx_object)
+  },
+  /** file.writeText */
+
+  writeText(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    const quick_text = quick_object.text
+    const quick_encoding = quick_object.encoding || UTF - 8
+    const quick_append = quick_object.append || false
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.filePath = quick_uri
+    }
+    if (quick_text) {
+      wx_object.data = quick_text
+    }
+    if (quick_encoding) {
+      wx_object.encoding = quick_encoding
+    }
+    wx_object.success = function (wx_res) {
+      const quick_res = {}
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().writeFile(wx_object)
+  },
+  /** file.writeArrayBuffer */
+
+  writeArrayBuffer(quick_object) {
+    return console.log('暂不支持！')
+  },
+  /** file.readText */
+
+  readText(quick_object) {},
+  /** file.readArrayBuffer */
+
+  readArrayBuffer(quick_object) {},
+  /** file.access */
+
+  access(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    // ////////////////////////////////////
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.path = quick_uri
+    }
+    wx_object.success = function (wx_res) {
+      const quick_res = {}
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().access(wx_object)
+  },
+  /** file.mkdir */
+
+  mkdir(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    const quick_recursive = quick_object.recursive || false
+    quick_object = null
+    // /////////////////////////////////////////////
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.dirPath = quick_uri
+    }
+    if (quick_recursive) {
+      wx_object.recursive = quick_recursive
+    }
+    wx_object.success = function () {
+      const quick_res = {}
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().mkdir(wx_object)
+  },
+  /** file.rmdir */
+
+  rmdir(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_success = quick_object.success
+    const quick_fail = quick_object.fail
+    const quick_complete = quick_object.complete
+    const quick_uri = quick_object.uri
+    const quick_recursive = quick_object.recursive || false
+    quick_object = null
+    // /////////////////////////////////////////////
+    const wx_object = {}
+    if (quick_uri) {
+      wx_object.dirPath = quick_uri
+    }
+    if (quick_recursive) {
+      wx_object.recursive = quick_recursive
+    }
+    wx_object.success = function () {
+      const quick_res = {}
+      if (quick_success) {
+        quick_success(quick_res)
+      }
+      if (quick_complete) {
+        quick_complete(quick_res)
+      }
+    }
+    wx_object.fail = function (wx_res) {
+      if (quick_fail) {
+        quick_fail(wx_res)
+      }
+      if (quick_complete) {
+        quick_complete(wx_res)
+      }
+    }
+    wx.getFileSystemManager().rmdir(wx_object)
+  }
+}
---
+++ src/api/system/router.js
@@ -0,0 +1,111 @@
+/* eslint-disable camelcase */
+import URL from 'oneutil/URL'
+
+module.exports = {
+  /*  network.getType/// */
+  push(quick_object) {
+    if (!quick_object) {
+      return
+    }
+    const quick_uri = quick_object.uri
+    const quick_params = quick_object.params
+    // ///////////////
+    const wx_object = {}
+    const url = new URL(quick_uri)
+    console.log(url)
+    if (url.scheme) {
+      switch (url.scheme) {
+        case 'tel':
+          wx.makePhoneCall({
+            phoneNumber: url.host,
+          })
+          break
+        case 'sms':
+          wx.showModal({
+            title: '不支持',
+            content: '微信小程序暂不支持发短信',
+          })
+          break
+        case 'http':
+        case 'https':
+          wx.navigateTo({
+            url: `/onekit/page/router.push/ie?url=${encodeURI(quick_uri)}`
+          })
+          break
+        case 'internal':
+          wx.showModal({
+            title: '带配置',
+            content: '请配置要打开的App',
+          })
+          break
+        case 'hap':
+          switch (url.host) {
+            case 'app':
+              wx.showModal({
+                title: '带配置',
+                content: '请配置要打开的小程序',
+              })
+              break
+            case 'settings':
+              break
+            default:
+              throw new Error(url.host)
+          }
+          // wx.showModal({
+          //   title: '不支持',
+          //   content: '微信小程序暂不支持',
+          // });
+          break
+        default:
+          throw new Error(url.scheme)
+      }
+    } else {
+      if (quick_params) {
+        wx_object.url = quick_uri + `?params=${quick_params.body}`
+      } else {
+        wx_object.url = quick_uri
+      }
+      wx.navigateTo(wx_object)
+    }
+  },
+  /** router.replace */
+
+  replace(quick_object) {
+    const quick_uri = quick_object.uri
+    // ///////////////
+    const wx_object = {}
+    const url = new URL(quick_uri)
+    wx_object.url = url
+    wx.redirectTo(wx_object)
+  },
+  /** router.back */
+
+  back(quick_object) {
+    const quick_uri = quick_object.uri
+    // ///////////////
+    const wx_object = {}
+    const url = new URL(quick_uri)
+    wx_object.url = url
+    wx.navigateBack(wx_object)
+  },
+  /** router.clear() */
+
+  clear() {
+    //  const wx_object = {}
+  },
+  /** router.getLength */
+
+  getLength() {
+    //  const wx_object = {}
+  },
+  /** router.getState */
+
+  getState() {
+    //  const wx_object = {}
+  },
+  /** router.getPages */
+
+  getPages() {
+    //  const wx_object = {}
+  }
+}
